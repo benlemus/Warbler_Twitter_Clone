463f8826cef2731600904baf7320d570bb6288ef
Ben Lemus <benlemus@Bens-MBP-2.lan>
2025-07-11 12:47:19 -0700

    added edit profile functionality in /users/profile route, uses password to authenticate but cannot chane password.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
-from forms import UserAddForm, LoginForm, MessageForm
+from forms import UserAddForm, LoginForm, MessageForm, UserEditForm
 from models import db, connect_db, User, Message
 
 CURR_USER_KEY = "curr_user"
@@ -18,7 +18,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 toolbar = DebugToolbarExtension(app)
 
@@ -216,6 +216,32 @@ def profile():
     """Update profile for current user."""
 
     # IMPLEMENT THIS
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+    
+    cur_u = User.query.get_or_404(session[CURR_USER_KEY])
+
+    form = UserEditForm(obj=cur_u)
+    
+    if form.validate_on_submit():
+
+        auth = User.authenticate(cur_u.username, form.password.data)
+
+        if auth:
+            cur_u.username = form.username.data
+            cur_u.email = form.email.data
+            cur_u.image_url = form.image_url.data
+            cur_u.header_image_url = form.header_image_url.data
+            cur_u.bio = form.bio.data
+
+            db.session.commit()
+
+            return redirect(f'/users/{cur_u.id}')
+        flash('Incorrect username or password', 'danger')
+        return redirect('/')
+
+    return render_template('users/edit.html', form=form)
 
 
 @app.route('/users/delete', methods=["POST"])
--- forms.py
+++ forms.py
@@ -23,3 +23,16 @@ class LoginForm(FlaskForm):
 
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[Length(min=6)])
+
+class UserEditForm(FlaskForm):
+    '''Form for editing a user profile'''
+
+    username = StringField('Username')
+    email = StringField('E-mail', validators=[Email()])
+    image_url = StringField('Image URL')
+    header_image_url = StringField('Header Image URL')
+    bio = TextAreaField('Bio')
+
+    password = PasswordField('Password')
+
+
