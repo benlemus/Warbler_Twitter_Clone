553cdd3c86faedbb3f08ec56657c3c8dd11605c2
Ben Lemus <benlemus@Bens-MBP-2.lan>
2025-07-18 22:19:48 -0700

    added teardown in user model tests, tested repr in user model and tested is_following method.

--- test_user_model.py
+++ test_user_model.py
@@ -17,10 +17,11 @@ from models import db, User, Message, Follows
 
 os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
 
-
 # Now we can import app
 
 from app import app
+
+app.config['WTF_CSRF_ENABLED'] = False
 
 # Create our tables (we do this here, so we only create the tables
 # once for all tests --- in each test, we'll delete the data
@@ -39,6 +40,8 @@ class UserModelTestCase(TestCase):
         Message.query.delete()
         Follows.query.delete()
 
+        db.session.commit()
+
         self.client = app.test_client()
 
     def test_user_model(self):
@@ -56,3 +59,82 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+
+    def tearDown(self):
+        User.query.delete()
+        Message.query.delete()
+        Follows.query.delete()
+
+        db.session.commit()
+    
+    def test_repr(self):
+        '''Does the __repr__ work correctly?'''
+        u = User(
+            email="test1@test.com",
+            username="testuser1",
+            password="HASHED_PASSWORD"
+        )
+
+        db.session.add(u)
+        db.session.commit()
+        
+        self.assertEqual(str(u), f"<User #{u.id}: {u.username}, {u.email}>")
+
+    def test_is_following(self):
+        '''Does the is_following model function dectect an added follow?'''
+
+        plain_password = 'password123'
+
+        # signs up user 1
+        signup_data1 = {'username': 'testuser1', 'password': plain_password, 'email': 'test@test.com', 'image_url': ''}
+        signup1 = self.client.post('/signup', data=signup_data1, follow_redirects=False)
+        self.assertEqual(signup1.status_code, 302)
+
+        # signs up user 2
+        signup_data2 = {'username': 'testuser2', 'password': plain_password, 'email': 'test2@test.com', 'image_url': ''}
+        signup2 = self.client.post('/signup', data=signup_data2, follow_redirects=False)
+        self.assertEqual(signup2.status_code, 302)
+
+        # checks both users valid sign up
+        u1 = User.query.filter_by(username=signup_data1['username']).first()
+        u2 = User.query.filter_by(username=signup_data2['username']).first()
+        self.assertIsNotNone(u1)
+        self.assertIsNotNone(u2)
+
+        # logs in user 1
+        login_data = {'username': u1.username, 'password': plain_password}
+        login = self.client.post('/login', data=login_data, follow_redirects=False)
+        self.assertEqual(login.status_code, 302)
+
+        # re gets user 2
+        u1 = User.query.filter_by(username=signup_data1['username']).first()
+        u2 = User.query.filter_by(username=signup_data2['username']).first()
+
+        # checks if u1 is following u2 before follow route
+        self.assertFalse(u1.is_following(u2))
+
+        # makes user 1 follow user 2
+        follow = self.client.post(f'/users/follow/{u2.id}', follow_redirects=False)
+        self.assertEqual(follow.status_code, 302)
+
+        # re gets user 2
+        u1 = User.query.filter_by(username=signup_data1['username']).first()
+        u2 = User.query.filter_by(username=signup_data2['username']).first()
+
+        # checks if u1 is following u2 after follow route
+        self.assertTrue(u1.is_following(u2))
+        
+        # has u1 unfollow u2
+        stop_following = self.client.post(f'/users/stop-following/{u2.id}', follow_redirects=False)
+        self.assertEqual(stop_following.status_code, 302)
+
+        # re gets users 1 and 2
+        u1 = User.query.filter_by(username=signup_data1['username']).first()
+        u2 = User.query.filter_by(username=signup_data2['username']).first()
+
+        # checks if u1 is no longer on u2 follow list
+        self.assertFalse(u1.is_following(u2))
+
+    
+    def test_is_followed_by(self):
+        pass
